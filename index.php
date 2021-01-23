f66cc5e50ed5ceaee39db4c5f8c0784c59b3b319
Jotta Ve <jvalexandre.sd@gmail.com>
2021-01-23 13:20:13 -0300

    Devotional 2021-01-23

--- index.php
+++ index.php
@@ -13,13 +13,16 @@ $version = "nvi";
 $abbrev = filter_input(INPUT_GET, "abbrev");
 $chapter = filter_input(INPUT_GET, "chapter");
 
-require __DIR__ . "/views/form.php";
+require __DIR__ . "/views/formSearchAPI.php";
 
 if($abbrev && $chapter){
     $url = "https://www.abibliadigital.com.br/api/verses/{$version}/{$abbrev}/{$chapter}";
     
     $json = json_decode(file_get_contents($url));
+    echo "<h1>{$abbrev} - {$chapter}</h1>";
     foreach($json->verses as $verse){
         echo "<p><sup>{$verse->number}</sup> {$verse->text}</p>";
     }
 }
+
+require_once __DIR__ . "/views/formDevotional.php";
---
+++ views/formDevotional.php
@@ -0,0 +1,7 @@
+<form action="" method="post">
+    <label for="devotional_date">Data:</label>
+    <input type="date" name="devotional_date" id="devotional_date">
+    <label for="devotional_content"></label>
+    <textarea name="devotional_content" id="devotional_content" cols="30" rows="10"></textarea>
+    <button type="submit">Salvar</button>
+</form>
